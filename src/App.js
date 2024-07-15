9d7aaa3584e590e19a218ac430f9b5c4c599e4d6
davidchen-signifyd <david.chen@signifyd.com>
2024-07-15 11:21:28 +0100

    create calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  const handleClear = () => {
+    setInput("");
+    setResult("");
+  };
+
+  const handleDelete = () => {
+    setInput(input.slice(0, -1));
+  }
+
+  const handleCalculate = () => {
+    try {
+      //I found out eval is dangerous and the use of a math library is needed but I cannot find a math library compatible with node version 14.
+      setResult(eval(input));
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+
+      <h1>Simple calculator</h1>
+
+      <div className="display">
+        <div className="input">{input}</div>
+        <div className="result">{result}</div>
+      </div>
+
+      <div className="buttons">
+        <button onClick={handleClear}>C</button>
+        <button className="span-two" onClick={ handleDelete }>DEL</button>
+        <button onClick={() => handleClick("+")}>+</button>
+
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button> 
+        <button onClick={() => handleClick("-")}>-</button>
+
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+        <button onClick={() => handleClick("*")}>*</button>
+
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
+        <button onClick={() => handleClick("/")}>/</button>
+
+        <button onClick={() => handleClick("0")}>0</button>
+        <button onClick={() => handleClick(".")}>.</button>
+        <button className="span-two" onClick={ handleCalculate }>=</button>
+      </div>
+
     </div>
   );
-}
+};
 
 export default App;
